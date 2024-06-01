9edf0785966d9f38a1d0236ba79a66e65b52d1a6
RyoGreen <ryo.nohara@casting-one.jp>
2024-06-01 20:08:29 +0900

    separate file

---
+++ rust/src/db/mod.rs
@@ -0,0 +1,20 @@
+use crate::config::DatabaseConfig;
+use tokio_postgres::{Client, Error, NoTls};
+
+pub async fn connect(db_config: &DatabaseConfig) -> Result<Client, Error> {
+    let (client, connection) = tokio_postgres::connect(
+        &format!(
+            "host={} user={} password={} port={} dbname={}",
+            db_config.host, db_config.user, db_config.password, db_config.port, db_config.dbname
+        )
+        .to_string(),
+        NoTls,
+    )
+    .await?;
+    tokio::spawn(async move {
+        if let Err(e) = connection.await {
+            eprintln!("connection error: {}", e);
+        }
+    });
+    Ok(client)
+}
